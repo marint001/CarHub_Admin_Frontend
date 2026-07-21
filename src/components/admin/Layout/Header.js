d7eb8e6f54ef7d7b9b042b67916a91c890a219a2
Visal <yanvisal513@gmail.com>
2026-07-21 20:07:28 +0700

    update

--- src/components/admin/Layout/Header.js
+++ src/components/admin/Layout/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FaBars, FaSearch, FaShieldAlt } from 'react-icons/fa';
+import { FaBell, FaSearch, FaShieldAlt } from 'react-icons/fa';
 import { useAuth } from '../../../context/AuthContext';
 import NotificationDropdown from '../../common/NotificationDropdown';
 
-const Header = ({ sidebarOpen, setSidebarOpen }) => {
+const Header = () => {
   const { user } = useAuth();
 
-  // Get user initials
   const getInitials = (name) => {
     if (!name) return 'A';
     return name.split(' ').map(n => n[0]).join('').toUpperCase().slice(0, 2);
@@ -15,13 +14,6 @@ const Header = ({ sidebarOpen, setSidebarOpen }) => {
   return (
     <header className="header">
       <div className="header-left">
-        <button 
-          className="menu-toggle" 
-          onClick={() => setSidebarOpen(!sidebarOpen)}
-        >
-          <FaBars />
-        </button>
-        
         <div className="search-box">
           <FaSearch className="search-icon" />
           <input type="text" placeholder="Search cars, leads..." />
@@ -29,7 +21,6 @@ const Header = ({ sidebarOpen, setSidebarOpen }) => {
       </div>
 
       <div className="header-right">
-        {/* Notification Dropdown */}
         <NotificationDropdown />
         
         <div className="user-profile">
